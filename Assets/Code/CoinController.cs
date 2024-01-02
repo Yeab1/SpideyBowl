8dce688e4ec9015dd856985a4addcb870543203c
Yeabsira Lemma <yeabsiralemma@Yeabsiras-Air.lan>
2024-01-02 10:25:38 -0800

    remove test logs

--- Assets/Code/CoinController.cs
+++ Assets/Code/CoinController.cs
@@ -17,7 +17,6 @@ public class CoinController : MonoBehaviour
         if (other.gameObject.GetComponent<BowlController>())
         {
             BowlController.instance.coinCount++;
-            Debug.Log("here");
             StartCoroutine(DestroyAfterAnimation());
         }
     }
@@ -25,12 +24,11 @@ public class CoinController : MonoBehaviour
     IEnumerator DestroyAfterAnimation()
     {
         animator.SetBool("isCollected", true);
-        Debug.Log("starting animation");
 
         // Wait for the animation duration
-        yield return new WaitForSecondsRealtime(animator.runtimeAnimatorController.animationClips[0].length);
+        yield return new WaitForSecondsRealtime(
+            animator.runtimeAnimatorController.animationClips[0].length);
 
-        Debug.Log("Destroying");
         Destroy(gameObject);
     }
 }
